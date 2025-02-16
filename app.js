da29d7ce6b4d26bb2a3c030140b60ddd83bf39bc
ricards <ricards.taukuls@inbox.lv>
2025-02-16 10:47:32 +0200

    feature: initialize the scene

---
+++ app.js
@@ -0,0 +1,20 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js'
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'https://cdn.skypack.dev/gsap'
+
+const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+camera.position.z = 13
+
+const scene = new THREE.Scene()
+let bee
+const loader = new GLTFLoader()
+loader.load(
+	'/bee_minecraft.glb',
+	function (gltf) {
+		bee = gltf.scene
+		scene.add(bee)
+	},
+	function (xhf) {},
+	function (err) {}
+)
